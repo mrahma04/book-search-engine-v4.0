2c22c1b0caeed914884223618532ceefcc347420
Mohammad Rahman <mbrahman@gmail.com>
2022-05-31 06:58:16 -0400

    added mutation to create user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,16 @@
+const { User } = require("../models");
+
 const resolvers = {
   Query: {
     helloWorld: () => {
       return "Hello world!";
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args.input);
+      return user
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,38 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        _id: ID
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    input UserInput {
+        username: String
+        email: String
+        password: String
+    }
+
     type Query {
         helloWorld: String
+    }
+
+    type Mutation {
+        addUser(input: UserInput!): User
     }
 `
 
